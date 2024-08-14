6a069e2c40d68d0be591e23a48b70db2fc1e0db4
Ashley Goad <agoad@cleverdevices.com>
2024-08-14 08:40:31 -0400

    Add build info

---
+++ W500Core/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace W500Core
+{
+    public static class Utils
+    {
+        private static DateTime GetBuildDate(Assembly assembly)
+        {
+            var attribute = assembly.GetCustomAttribute<BuildDateAttribute>();
+            return attribute?.DateTime ?? default(DateTime);
+        }
+
+        public static string GetBuildName()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var dt = GetBuildDate(assembly);
+            return assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title + "-" + dt.ToString("yyyyMMddHHmmss");
+        }
+    }
+}
